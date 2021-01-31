6e9043025a922d8f7b130bb6928ea04c5bb12b97
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-01-31 21:47:11 +0700

    Creat view item + table

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -9,5 +9,9 @@ module.exports = {
 
     viewBank: (req, res) => {
         res.render('admin/bank/viewBank.ejs');
+    },
+
+    viewItem: (req, res) => {
+        res.render('admin/item/viewItem.ejs');
     }
 };
--- routes/admin.js
+++ routes/admin.js
@@ -6,5 +6,6 @@ const adminController = require('../controllers/adminController');
 router.get('/dashboard', adminController.viewDasboard);
 router.get('/category', adminController.viewCategory);
 router.get('/bank', adminController.viewBank);
+router.get('/item', adminController.viewItem);
 
 module.exports = router;
