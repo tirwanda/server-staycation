88539557408d623735520869e9fd9c5b9477f7b7
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-01-31 21:02:32 +0700

    Create view category + table

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,9 @@
 module.exports = {
     viewDasboard: (req, res) => {
         res.render('admin/dashboard/viewDashboard.ejs');
+    },
+
+    viewCategory: (req, res) => {
+        res.render('admin/category/viewCategory.ejs');
     }
 };
--- routes/admin.js
+++ routes/admin.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 
 router.get('/dashboard', adminController.viewDasboard);
+router.get('/category', adminController.viewCategory);
 
 module.exports = router;
